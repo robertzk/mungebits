7e62a38fc599a5e99c3f82b10fa32756a0d96171
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-01-02 16:30:23 -0600

    added support for mutating inputs in column_transformation

--- R/column_transformation.r
+++ R/column_transformation.r
@@ -8,6 +8,9 @@
 #'
 #' @param transformation a function. The only argument should be the original
 #'    column.
+#' @param mutating a logical. Announces whether the transformation passed in
+#'    attempts to maintain state between prediction and training runs using
+#'    the "inputs" global. (see also \code{\link{mungebit}})
 #' @return a function which takes a data.frame and a vector of columns and
 #'    applies the transformation.
 #' @seealso \code{\link{multi_column_transformation}}
@@ -16,23 +19,52 @@
 #' doubler <- column_transformation(function(x) 2*x)
 #' # doubles the Sepal.Length column in the iris dataset
 #' doubler(iris, c('Sepal.Length')) 
-column_transformation <- function(transformation) {
-  force(transformation)
+column_transformation <- function(transformation, mutating = FALSE) {
+  force(transformation); force(mutating)
   invisible(function(dataframe, cols = colnames(dataframe), ...) {
     # The fastest way to do this. The alternatives are provided in the comment below
     assign("*tmp.fn.left.by.mungebits.library*",
            transformation, envir = parent.frame())
+    mutating <- mutating # Copy from parent scope to this environment
     cols <- force(cols)
     if (is.logical(cols)) cols <- which(cols)
+    colns <- if (is.character(cols)) cols else colnames(dataframe)[cols]
 
     invisible(eval(substitute({
       # Trick to make assignment incredibly fast. Could screw up the
       # data.frame if the function is interrupted, however.
       class(dataframe) <- 'list'
       on.exit(class(dataframe) <- 'data.frame')
-      environment(`*tmp.fn.left.by.mungebits.library*`) <- environment()
-      dataframe[cols] <- lapply(dataframe[cols],
-                                `*tmp.fn.left.by.mungebits.library*`, ...)
+      if (!mutating) {
+        environment(`*tmp.fn.left.by.mungebits.library*`) <- environment()
+        dataframe[cols] <- lapply(dataframe[cols],
+          `*tmp.fn.left.by.mungebits.library*`, ...)
+      } else {
+        # We must now be surgically precise. The transformation function
+        # is attempting to store values in "inputs" using inputs <<- ...
+        # However, we would like to allow different values for different
+        # columns (for example, a mean imputer needs to remember the mean
+        # for each column separately). Therefore, we set an "inputs" variable
+        # locally and re-assign the scope of the transformation so that the <<-
+        # operator modifies this "inputs" rather than the one in a parent
+        # scope (like in a mungebit). Afterwards, we exploit the fact that the
+        # <<- operator never modifies local scope using
+        #   inputs[[column_name]] <<- inputs
+        dataframe[cols] <- lapply(colns, function(column_name) {
+          inputs <- NULL
+          environment(`*tmp.fn.left.by.mungebits.library*`) <- environment()
+          column <- `*tmp.fn.left.by.mungebits.library*`(dataframe[[column_name]], ...)
+          if (!is.null(inputs)) {
+            # The <<- operator never modifies local scope so that left "inputs"
+            # refers to the parent.frame() whereas the right "inputs" refers
+            # to the one in local scope. The end result is that if the column
+            # names were, e.g., c('age', 'height'), the "inputs" in the parent
+            # scope would end up as a list with an $age and $height value.
+            inputs[[column_name]] <<- inputs
+          }
+          column
+        })
+      }
       # Slightly slower is:
       # for(i in cols) dataframe[[i]] <-
       #   `*tmp.fn.left.by.mungebits.library*`(dataframe[[i]], ...)
@@ -56,5 +88,4 @@ column_transformation <- function(transformation) {
 # 5: The method above for dynamic lambdas
 # An extra rm function after the assign increases runtime by 75% with frequent application.
 # The fifth option is the fastest.
-
 
