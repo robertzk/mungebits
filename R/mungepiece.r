27a563d12691c9925fe96093cdf620a4a13436e2
RobertZK <technoguyrob@gmail.com>
2014-05-05 08:46:14 -0500

    tests for list_merge

--- R/mungepiece.r
+++ R/mungepiece.r
@@ -55,15 +55,6 @@ mungepiece <- setRefClass('mungepiece',
 )
 
 is.mungepiece <- function(x) inherits(x, 'mungepiece')
-
-list_merge <- function(list1, list2) {
-  for (i in seq_along(list2)) {
-    name <- names(list2)[i]
-    if (!identical(name, NULL) && !identical(name, "")) list1[[name]] <- list2[[i]]
-    else list1 <- append(list1, list(list2[[i]]))
-  }
-  list1
-}
     
 # S3 definition... uglier I think, since you need to knwo the calling convention
 #mungepiece <- function(bit, train_args, predict_args = train_args) {
---
+++ inst/tests/test-list_merge.r
@@ -0,0 +1,16 @@
+context('list_merge')
+
+test_that('it can merge NULL with non-null', {
+  expect_identical(list_merge(NULL, list(a = 1)), list(a = 1))
+})
+
+test_that('it can merge a simple example', {
+  expect_identical(list_merge(list(a = 1, b = 2), list(b = 3, c = 4)),
+                   list(a = 1, b = 3, c = 4))
+})
+
+test_that('it can merge with NULL', {
+  expect_identical(list_merge(list(a = 1), NULL), list(a = 1))
+  expect_identical(list_merge(NULL, NULL), list())
+})
+
